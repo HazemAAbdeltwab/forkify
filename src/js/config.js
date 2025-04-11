f07b391cf7e41f676590715f4845166f294c306f
Hazem Ahmed <hazemahmedabdeltwab123@gmail.com>
2025-04-11 13:13:21 +0200

    Add project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4b7b6b19-c79b-44d6-84e0-5998d651561e';
+export const MODAL_CLOSE_SEC = 2.5;
